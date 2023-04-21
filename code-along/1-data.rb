6b7caccaa6a224bded8a42787a4b84a6f84b7484
Huong Vu <huong.vu@kellogg.northwestern.edu>
2023-04-21 18:41:42 +0000

    Commit please

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -3,11 +3,13 @@
 # ruby 1-data.rb
 
 # In Ruby, there are different *types* of data:
-
+puts 5
+puts 3
 # Numbers
 
 # Perform simple math with numbers
-
+puts 5*3
+puts 5.0/3.0
 # Strings
 
 # Combine strings together

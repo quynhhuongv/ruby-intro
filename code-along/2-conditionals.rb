f54720e9a3a15fc82302d1231b8b07060e44f92a
Huong Vu <huong.vu@kellogg.northwestern.edu>
2023-04-21 19:33:44 +0000

    please commit

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,44 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+this_is_true = true
+this_is_false = false
+number = 5
 
 # Boolean Expressions
+puts 3 == 2
+puts number ==5
+puts number > 10
+puts number !=5
 
 # If Conditional Logic
+if  3>2
+    puts "yes it is"
+end 
 
 # If/Else Conditional Logic
+user_entered_password = "tacos"
+real_password = "secret"
+if user_entered_password == real_password
+    puts "you're in"
+else 
+    puts "no way!"
+end 
 
 # Elsif Conditional Logic
+your_score = 2
+opponent_score = 2
+if your_score > opponent_score
+    puts "yay"
+elseif your_score == opponent_score
+puts "ok you pay"
+else 
+    puts "sadness"
+end 
 
 # Combining Expressions
+temp = 68
+precip = 0
+if temp >= 65 && temp <=90 && precip == 0
+    puts "it's nice"
+end 
--- labs/2-conditionals.rb
+++ labs/2-conditionals.rb
@@ -30,3 +30,11 @@ puts die2
 # 3. create a variable for the total and write it to the screen
 total = die1 + die2
 puts "The total is: #{total}"
+
+if total == 7 || total = 11
+    puts "YOU WIN"
+elseif total == 2 || total == 3 || total == 12
+puts "YOU LOST"
+else 
+    puts "The point is {total}"
+end 
